e3219c3c6507bffadcc5dd7eda75d79549d2aeed
Arun Ramesh <arunrameshmakattil1993@gmail.com>
2024-03-30 17:51:53 +0000

    Implemented sorting functionality for names and added corresponding unit test cases.

---
+++ Name-Sorter/Interfaces/INameSorter.cs
@@ -0,0 +1,14 @@
+﻿using Name_Sorter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Name_Sorter.Interfaces
+{
+    public interface INameSorter
+    {
+        List<Name> SortNames(List<Name> names);
+    }
+}
---
+++ Name-Sorter/Services/NameSorter.cs
@@ -0,0 +1,22 @@
+﻿using Name_Sorter.Interfaces;
+using Name_Sorter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Name_Sorter.Services
+{
+    public class NameSorter : INameSorter
+    {
+        public List<Name> SortNames(List<Name> names)
+        {
+            List<Name> sortedList=new List<Name>();
+            sortedList = names.OrderBy(n => n.LastName)
+                              .ThenBy(n => string.Join(" ", n.GivenNames))
+                              .ToList();
+            return sortedList;
+        }
+    }
+}
