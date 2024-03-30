48f6a74f649f487673aa69c35bcf4e4671b6f316
Arun Ramesh <arunrameshmakattil1993@gmail.com>
2024-03-30 20:58:01 +0000

    Added comments to the SortNames method.

--- Name-Sorter/Services/NameSorter.cs
+++ Name-Sorter/Services/NameSorter.cs
@@ -10,12 +10,15 @@ namespace Name_Sorter.Services
 {
     public class NameSorter : INameSorter
     {
+        // Method to sort a list of names
         public List<Name> SortNames(List<Name> names)
         {
             List<Name> sortedList=new List<Name>();
+            // Sort the names first by last name, then by given names
             sortedList = names.OrderBy(n => n.LastName)
                               .ThenBy(n => string.Join(" ", n.GivenNames))
                               .ToList();
+            // Return the sorted list of names
             return sortedList;
         }
     }
